8fc88fd2661e0ceb93ee9f82cf7898649638f969
kundururamu <42113924+kundururamu@users.noreply.github.com>
2022-12-06 19:06:01 -0600

    Add files via upload

---
+++ Login.js
@@ -0,0 +1,68 @@
+import {useState} from 'react'
+import { Link } from 'react-router-dom'
+import './forms.css'
+import {signInWithEmailAndPassword, sendEmailVerification} from 'firebase/auth'
+import {auth} from './firebase'
+import {useNavigate} from 'react-router-dom'
+import {useAuthValue} from './AuthContext'
+
+
+function Login(){
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('') 
+  const [error, setError] = useState('')
+  const {setTimeActive} = useAuthValue()
+  const navigate = useNavigate()
+
+  const login = e => {
+    e.preventDefault()
+    signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      if(!auth.currentUser.emailVerified) {
+        sendEmailVerification(auth.currentUser)
+        .then(() => {
+          setTimeActive(true)
+          navigate('/verify-email')
+        })
+      .catch(err => alert(err.message))
+    }else{
+      navigate('/')
+    }
+    })
+    .catch(err => setError(err.message))
+  }
+
+  return(
+    <div className='center'>
+      <div className='auth'>
+        <h1>UTA COURSE AVAILABILITY</h1>
+        <h1>Student Login</h1>
+        {error && <div className='auth__error'>{error}</div>}
+        <form onSubmit={login} name='login_form'>
+          <input 
+            type='email' 
+            value={email}
+            required
+            placeholder=" email_id"
+            onChange={e => setEmail(e.target.value)}/>
+
+          <input 
+            type='password'
+            value={password}
+            required
+            placeholder='password'
+            onChange={e => setPassword(e.target.value)}/>
+
+          <button type='submit'>Login</button>
+        </form>
+        <p>
+          Don't have an account? 
+          <Link to='/register'>Register_Here</Link>
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Login
